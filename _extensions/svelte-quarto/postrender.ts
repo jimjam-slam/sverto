cf2da3793e9bc4cfa229801bca584bd53372ba45
James Goldie <james.goldie@360info.org>
2023-01-11 15:28:44 +1100

    Add planning note to postrender

---
+++ _extensions/svelte-quarto/postrender.ts
@@ -0,0 +1,5 @@
+// now we have a list of svelte components to compile
+// (with potential duplicates) in .quarto/.svelte-quarto-matches.
+
+// we need to compile them to .js files and get them to expected place in
+// the output folder
