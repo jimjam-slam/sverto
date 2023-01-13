68630e36025a7b0aeba0659fde7133f55b9defcd
James Goldie <james.goldie@360info.org>
2023-01-13 18:20:59 +1100

    Fix folder refresh

--- _extensions/sverto/refresh.ts
+++ _extensions/sverto/refresh.ts
@@ -1,4 +1,2 @@
 // delete the temporary import block files and the temp render metadata
-Deno.remove(".sverto/import",  { recursive: true })
-Deno.remove(".sverto/.sverto-imports")
-Deno.remove(".sverto/.sverto-outdir")
+Deno.remove(".sverto/",  { recursive: true })
