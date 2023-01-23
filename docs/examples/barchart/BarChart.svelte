8da9bde397dcdee2442a867413a90f7f062cb06f
James Goldie <james.goldie@360info.org>
2023-01-23 17:35:18 +1100

    Add bar chart example scaffold

---
+++ docs/examples/barchart/BarChart.svelte
@@ -0,0 +1,23 @@
+<script>
+  // let's borrow svelte's fly transitions for the circles that need to be
+  // created or destroyed
+  // https://svelte.dev/tutorial/transition
+  // import { fly } from "svelte/transition";
+
+  // here we declare `data` as a prop that this component can expect
+  // https://svelte.dev/tutorial/declaring-props
+  // export let data;
+
+  // prefix a statement with $: to make it reactive (so it runs every time
+  // data changes)
+  // https://svelte.dev/tutorial/reactive-statements
+  // $: console.log("Dataset prop:", data);
+
+</script>
+
+<!-- we use svelte's in/out transitions for entering and leaving dom elements,
+     and vanilla css transitions for retained elements that change. the
+     #each block means we create an svg <circle> for each element of data -->
+<svg>
+  <text cx="50%" cy ="50%">Hello world!</text>
+</svg>
