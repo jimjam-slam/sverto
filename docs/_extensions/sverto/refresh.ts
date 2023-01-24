185e5bb060d5c035ad3384d1b2abde3c7c263e4c
James Goldie <james.goldie@360info.org>
2023-01-24 11:22:02 +1100

    Remove debug statements from docs

--- docs/_extensions/sverto/refresh.ts
+++ docs/_extensions/sverto/refresh.ts
@@ -1,7 +1,5 @@
 // delete the temporary import block files and the temp render metadata
 
-console.log("Render all? " + Deno.env.get("QUARTO_PROJECT_RENDER_ALL"))
-
 if (Deno.env.get("QUARTO_PROJECT_RENDER_ALL") == "1") {
   console.log("Clearing .sverto/")
   try {
