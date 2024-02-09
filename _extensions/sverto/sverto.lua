16c5b67b1fac4c0fddb07aac415d186b2e4834a8
James Goldie <james@dicenerd.app>
2024-02-09 19:19:40 +1100

    First pass structure for filter to inject svelte js

---
+++ _extensions/sverto/sverto.lua
@@ -0,0 +1,105 @@
+
+function append_to_file(name, content)
+  local file = io.open(name, "a")
+  if file == nil then
+      return ""
+  end
+  file:write(content)
+  file:close()
+  return content
+end
+
+function inject_svelte(m)
+
+  quarto.log.warning("PROCESSING DOC")
+  quarto.log.warning("QUARTO_PROJECT_RENDER_ALL is ")
+  quarto.log.warning(os.getenv("QUARTO_PROJECT_RENDER_ALL"))
+  quarto.log.warning("QUARTO_PROJECT_OUTPUT_DIR is ")
+  quarto.log.warning(os.getenv("QUARTO_PROJECT_OUTPUT_DIR"))
+  quarto.log.warning("QUARTO_PROJECT_INPUT_FILES is ")
+  quarto.log.warning(os.getenv("QUARTO_PROJECT_INPUT_FILES"))
+  quarto.log.warning("QUARTO_PROJECT_OUTPUT_FILES is ")
+  quarto.log.warning(os.getenv("QUARTO_PROJECT_OUTPUT_FILES"))
+
+  quarto.log.warning("Path separator is:")
+  local sep = pandoc.path.separator
+
+  -- quarto.log.warning("DOC META:")
+  -- quarto.log.warning(m)
+
+  if not quarto.doc.isFormat('html') then
+    quarto.log.warning("Sverto shortcode ignored for non-HTML output")
+    return nil
+  end
+
+  -- no files to process? abort
+  if m.sverto == nil or m.sverto.use == nil then
+    quarto.log.warning("Define sverto.use in frontmatter")
+    return nil
+  end
+
+  -- quarto.log.warning("sverto.use:")
+  -- quarto.log.warning(m.sverto.use)
+
+  -- abort if sverto.use is not a list of MetaInlines
+  if pandoc.utils.type(m["sverto"]["use"]) ~= "List" then
+    quarto.log.error(
+      "sverto.use should be Inlines, not " .. 
+      pandoc.utils.type(m["sverto"]["use"]))
+  end
+
+  -- either add text to start of body (and return nil), or return a rawblock
+  -- %s: obj_name
+  -- %s: file_name, adapted for output path (and .svelte => .js)
+  local svelteInitTemplate = [[
+    <script>
+      // when the doc is ready, find quarto's ojs and inject svelte import
+      document.addEventListener("DOMContentLoaded", () => {
+        %s = await import("%s")
+
+        const ojsModule = window._ojs?.ojsConnector?.mainModule
+        if (ojsModule === undefined) {
+          console.error("Quarto OJS module not found")
+        }
+
+        // TODO - check to see if there's already a variable with that name
+        const svertoImport = ojsModule?.variable()
+        svertoImport?.define("%s", %s)
+      }
+    </script>
+  ]]
+
+  for _, path in ipairs(m.sverto.use) do
+    -- this is where we process the path
+
+    local in_path  = pandoc.utils.stringify(path)
+    local in_dir   = pandoc.path.directory(in_path)
+    local in_name  = pandoc.path.filename(in_path)
+    local obj_name = pandoc.path.split_extension(in_name)
+    local compiled_path = pandoc.path.join({
+      in_dir,
+      obj_name .. ".sverto.js"
+    })
+  
+    quarto.log.warning("Processing: " .. in_path ..
+      " => " .. compiled_path)
+
+    local svelteInsert = string.format(svelteInitTemplate,
+      obj_name, compiled_path, obj_name, obj_name)
+
+    quarto.log.warning("INJECTION:")
+    quarto.log.warning(svelteInsert)
+
+    quarto.doc.include_text("before-body", svelteInsert)
+
+    -- now run the svelte compiler... if we're not in a project
+    -- os.execute?
+    -- os.execute("")
+    quarto.log.warning("TODO - compile svelte imports")
+  end
+
+end
+
+return {
+  Meta = inject_svelte
+}
