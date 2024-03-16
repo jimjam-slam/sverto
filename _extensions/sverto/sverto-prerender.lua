94ae9a34d959cc3098a9d4b86574f165bf51588c
James Goldie <james@dicenerd.app>
2024-03-16 15:07:57 +1100

    WIP debugging prerender

--- _extensions/sverto/sverto-prerender.lua
+++ _extensions/sverto/sverto-prerender.lua
@@ -1,25 +1,19 @@
 
 -- return contents of named file
-function read_file(name)
-  local file = io.open(name, "r")
-  if file == nil then
-      return ""
-  end
-  local contents = file:read("a")
-  file:close()
-  return contents
-end
-
--- use mkdir (windows) or mkdir -p (*nix) to create directories
--- from https://stackoverflow.com/a/14425862/3246758
-function get_path_sep()
-  return package.config:sub(1, 1)
-end
+-- function read_file(name)
+--   local file = io.open(name, "r")
+--   if file == nil then
+--       return ""
+--   end
+--   local contents = file:read("a")
+--   file:close()
+--   return contents
+-- end
 
 -- get a prefix for calling npm run based on windows or *nix/macos
 -- using path separator: i don't know pandoc.system.os() values
 function get_cmd_prefix()
-  if get_path_sep() == "\\" then
+  if pandoc.path.separator == "\\" then
     return "cmd /c "
   else
     return ""
@@ -27,20 +21,25 @@ function get_cmd_prefix()
 end
 
 -- create a folder recursively (mkdir on windows, mkdir -p on *nix)
-function create_dir_recursively(path)
-  local path_separator = get_path_sep()
-  if path_separator == "\\" or path_separator == "\"" then
-    os.execute("mkdir " .. path)
-  else
-    -- macos/linux
-    os.execute("mkdir -p " .. path)
-  end
-end
+-- function create_dir_recursively(path)
+--   if pandoc.path.separator == "\\" or pandoc.path.separator == "\"" then
+--     os.execute("mkdir " .. path)
+--   else
+--     -- macos/linux
+--     os.execute("mkdir -p " .. path)
+--   end
+-- end
 
--- path_dir: extract the folder path from a file path
--- from https://stackoverflow.com/a/9102300/3246758
-function path_dir(path)
-  return path:match("(.*".. get_path_sep() ..")") or ""
+-- file_exists: true if the file at `name` exists
+-- from https://pandoc.org/lua-filters.html#building-images-with-tikz
+function file_exists(name)
+  local f = io.open(name, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
 end
 
 -- offset a relative svelte_path to be relative to the
@@ -56,7 +55,7 @@ function offset_svelte_path(svelte_path, input_path)
     pandoc.path.make_relative(svelte_path, input_path, false)
   quarto.log.info("Offsetting svelte path from qmd: " .. input_path .. " + " .. svelte_path .. " => " ..
     offset_path)
-  return offset_path
+  return tostring(offset_path)
 
 end
 
@@ -67,47 +66,59 @@ input_paths = os.getenv("QUARTO_PROJECT_INPUT_FILES")
 
 svelte_paths = {}
 
-for _, input_path in ipairs(input_paths) do
+-- for _, input_path in ipairs(input_paths) do
+for input_path in input_paths:gmatch("[^\n]+") do
 
-  quarto.log.warning("Looking for Svelte files in " .. input_path)
+  print("Looking for Svelte files in " .. input_path)
   local doc = pandoc.read(input_path, "markdown")
 
   doc:walk {
-    Meta = function (meta)
+    Meta = function (m)
+
+      if m.sverto == nil or m.sverto.use == nil then
+        print("No Sverto paths in this file")
+        return nil
+      end
 
       -- single string: add one path
       if type(m.sverto.use) == "string" then
-        svelte_paths:insert(m.sverto.use)
+        print("Adding single path: " .. m.sverto.use)
+        local offset_path = offset_svelte_path(m.sverto.use, input_path)
+        svelte_paths[offset_path] = offset_path
       end
 
-      -- list: add each path
+      -- list: add each unique path, offsetting it from its input_path .qmd
       if pandoc.utils.type(m.sverto.use) ~= "List" then
         for i, svelte_path in ipairs(m.sverto.use) do
-            svelte_paths:insert(tostring(svelte_path))
+          print("Adding path from list: " .. svelte_path)
+          local offset_path = offset_svelte_path(svelte_path, input_path)
+          svelte_paths[offset_path] = offset_path
         end
       end
+
+      return nil
 
     end
   }
   
 end
 
--- now offset paths and remove duplicates
--- (by assigning values to table keys)
-unique_svelte_paths = {}
-for _, svelte_path in ipairs(svelte_paths) do
-  local offset_path = offset_svelte_path(svelte_path, input_path)
-  unique_svelte_paths[offset_path] = offset_path
-end
-
 -- now concatenate them with : and send them to the svelte compiler
 svelte_path_string = ""
-for _, svelte_path in ipairs(unique_svelte_paths) do
+for _, svelte_path in ipairs(svelte_paths) do
   svelte_path_string = svelte_path_string .. svelte_path .. ":"
 end
 
 -- finally, call the svelte compiler via rollup
-rollup_config = quarto.utils.resolve_path("rollup.config.js")
+rollup_config = ""
+if file_exists("./_extensions/jimjam-slam/sverto/rollup.config.js") then
+  rollup_config = "./_extensions/jimjam-slam/sverto/rollup.config.js"
+elseif file_exists("./_extensions/sverto/rollup.config.js") then
+  rollup_config = "./_extensions/sverto/rollup.config.js"
+else
+  print("Error: no rollup config found. Is Sverto installed properly?")
+  os.exit(1)
+end
 
 cmd =
   get_cmd_prefix() ..
@@ -116,5 +127,8 @@ cmd =
   '--quarto-out-path="' .. os.getenv("QUARTO_PROJECT_OUTPUT_DIR") .. '" ' ..
   '--sverto-in-paths="' .. svelte_path_string .. '"'
 
+print("Calling Svelte compiler via rollup using command: \n\n")
+print(cmd)
+
 local svelteResult = os.execute(cmd)
-quarto.log.warning("Svelte compiler finished with code " .. svelteResult)
+print("Svelte compiler finished with code " .. svelteResult)
